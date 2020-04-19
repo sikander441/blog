9cba95353ca0bbeda6c6ed788fc9b43eb4b1370c
sikander441 <sikander441@gmail.com>
2020-04-19 14:04:44 +0530

    Added route to ceate new blogs

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express();
 const path = require('path')
 var hbs = require('express-handlebars');
+const fs = require('fs')
 app.set('view engine', 'hbs');
+var myParser = require("body-parser");
 app.use(express.static(path.join(__dirname, '/public')));
 
 const PORT = process.env.PORT || 3000
@@ -12,42 +14,51 @@ app.engine( 'hbs', hbs( {
     partialsDir: __dirname + '/views/partials/'
   }));
 
+  app.use(myParser.urlencoded({extended : true}));
 
-app.get('/',(req,res) =>{
-    blogPosts=[
-        {
-            title:'The Making of this Blog',
-            createdAt:'3:30 AM 2019/04/2020',
-            editedAt:'3:30 AM 2019/04/202',
-            blogSummary:' In this blog i describe how this Blog application was made. I used Materialize UI for CSS and for backend i used the Nodejs and mongoose.',
-            id:23
-        },
-        {
-            title:'Second Blog',
-            createdAt:'3:31 AM 2019/04/2020',
-            editedAt:'3:30 AM 20192/04/202',
-            blogSummary:' In this blog i describe how this Blog application was made. I used Materialize UI for CSS and for backend i used the Nodejs and mongoose.',
-            id:24
-        },
-        {
-            title:'Second Blog',
-            createdAt:'3:31 AM 2019/04/2020',
-            editedAt:'3:30 AM 20192/04/202',
-            blogSummary:' In this blog i describe how this Blog application was made. I used Materialize UI for CSS and for backend i used the Nodejs and mongoose.',
-            id:24
-        },
-        {
-            title:'Second Blog',
-            createdAt:'3:31 AM 2019/04/2020',
-            editedAt:'3:30 AM 20192/04/202',
-            blogSummary:' In this blog i describe how this Blog application was made. I used Materialize UI for CSS and for backend i used the Nodejs and mongoose.',
-            id:24
-        }
-    ]
+app.get('/',async (req,res) =>{
+    blogPosts = JSON.parse(fs.readFileSync('blogs.json'))
+
+    console.log(blogPosts)
     res.render('home',{
         blogPosts:blogPosts
     });
 })
+app.get('/blog/create',(req,res) => {
+
+    res.render('newBlog')
+
+})
+app.post('/blog/post',(req,res) => {
+    const title = req.body.title
+    const body = req.body.body
+    const id = req.body._id;
+    if(!id){
+        var blogPosts = JSON.parse(fs.readFileSync('blogs.json')) 
+        var _id = blogPosts.length+1
+        const blog ={
+            body,
+            title,
+            _id,
+            createdAt:new Date().toISOString().slice(0,10)
+        }
+        blogPosts.push(blog)
+        fs.writeFileSync('blogs.json',JSON.stringify(blogPosts))
+        res.send(blogPosts)
+
+    }
+
+})
+app.get('/blog/:id',(req,res) => {
+
+    var blogPosts = JSON.parse(fs.readFileSync('blogs.json')) 
+    const blog = blogPosts.find( (blog) => blog._id==req.params.id)
+    console.log(blog)
+    res.render('blog',{
+        blog:blog
+    });
+})
+
 
 app.listen(PORT,(err)=>{
     if(!err)
